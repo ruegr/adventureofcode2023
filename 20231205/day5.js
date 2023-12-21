e97027ec32603006b00c11e8ccd853cad668145b
Felix <8162132+ruegr@users.noreply.github.com>
2023-12-21 11:11:45 +0100

    working version of day5, part1

--- 20231205/day5.js
+++ 20231205/day5.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const TEST = true;
+const TEST = false;
 const DEBUG = false;
 
 const fs = require('node:fs');
@@ -8,8 +8,8 @@ let input = '';
 
 const populateInput = (TEST) => {
     if (TEST) {
-        input = 
-`seeds: 79 14 55 13
+        input =
+            `seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -41,10 +41,10 @@ temperature-to-humidity map:
 
 humidity-to-location map:
 60 56 37
-56 93 4`;    
+56 93 4`;
     } else {
         try {
-            input = fs.readFileSync('input', 'utf8');
+            input = fs.readFileSync('input.txt', 'utf8');
         } catch (err) {
             console.error(err);
             return;
@@ -52,6 +52,58 @@ humidity-to-location map:
     }
 }
 
+const seeds = {};
+const maps = {};
+
 populateInput(TEST);
 
-console.log(input);
+
+input.split`\n\n`.forEach((el) => {
+    const [key, vals] = el.split(/:\s/);
+    if (key == "seeds") {
+        vals.split` `.map((el) => el * 1).forEach((el) => seeds[el * 1] = {});
+        return;
+    }
+
+    const mapName = key.split` `[0];
+    maps[mapName] = [];
+
+    for (let line of vals.split`\n`) {
+
+        line = line.split` `.map((el) => el * 1);
+        maps[mapName].push({ destStart: line[0], destEnd: line[0] + line[2] - 1, srcStart: line[1], srcEnd: line[1] + line[2] - 1 });
+    }
+});
+
+
+const getMapping = (srcVal, mapName) => {
+    const theMap = maps[mapName].filter((m) => m['srcStart'] <= srcVal && m['srcEnd'] >= srcVal)[0];
+    if (theMap === undefined) return srcVal; //no explicit mapping --> mapping==srcVal
+
+    const start = theMap['srcStart'];
+    const delta = srcVal - theMap['srcStart'];
+
+    return theMap['destStart'] + delta;
+}
+
+Object.keys(seeds).forEach((seed) => {
+    seed = seed * 1;
+    let category = 'seed';
+    let srcVal = seed;
+    let mapName = ''
+    do {
+        mapName = Object.keys(maps).filter((mapName) => mapName.split`-`[0] == category)[0];
+        if (mapName === undefined) return;
+        const targetCategory = mapName.split`-`[2];
+
+        seeds[seed][targetCategory] = getMapping(srcVal, mapName);
+
+        srcVal = seeds[seed][targetCategory];
+        category = targetCategory;
+    } while (true)
+})
+
+
+
+const lowestLocation = Math.min(...Object.values(seeds).map((seed) => seed['location']));
+console.log(lowestLocation);
