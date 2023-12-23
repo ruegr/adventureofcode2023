e0fb93d7b68f078eb7858d7f89e827e484d221c5
Felix <8162132+ruegr@users.noreply.github.com>
2023-12-23 10:49:04 +0100

    working version of day7-pt1! additional test-input, compare https://www.reddit.com/r/adventofcode/comments/18cr4xr/2023_day_7_better_example_input_not_a_spoiler/

--- 20231207/day7.js
+++ 20231207/day7.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const TEST = true;
+const TEST = false;
 const DEBUG = false;
 
 const { assert, debug } = require('node:console');
@@ -9,7 +9,7 @@ let input = '';
 
 const populateInput = (TEST = false) => {
     try {
-        input = fs.readFileSync(TEST ? 'test-input.txt' : 'input.txt', 'utf8');
+        input = fs.readFileSync(TEST ? 'test-input enh.txt' : 'input.txt', 'utf8');
     } catch (err) {
         console.error(err);
         return;
@@ -18,8 +18,8 @@ const populateInput = (TEST = false) => {
 
 populateInput(TEST);
 
-console.log('******* INPUT')
-console.log(input);
+//console.log('******* INPUT')
+//console.log(input);
 
 let hands = [];
 
@@ -29,7 +29,7 @@ class Hand {
         this.bid = bid * 1;
         this.cards = [];
         cards.split``.forEach((c) => this.cards.push(new Card(c)));
-        this.cards.sort((a, b) => b.value - a.value);
+        //this.cards.sort((a, b) => b.value - a.value);
         //this.rank = [0,0,0,0,0,0]; //rank of: handtype, topcard, 2nd-topcard, [...], lowestcard. Later ranks may be 0 when hand consists of i.e. pairs
         [this.type, this.rank, this.cardRanks] = this.evaluate();
     }
@@ -40,7 +40,7 @@ class Hand {
         const uniqueCardChars = [...new Set(cardChars)];
         const uniqueCount = uniqueCardChars.map((c) => cardChars.filter((cc)=> cc===c).length).sort(sortReverse).join``;
 
-        const cardRanks = uniqueCardChars
+        /*const cardRanks = uniqueCardChars
             .map((c) => ({char:c, value:Card.cardVals[c][1], count: cardChars.filter(cc => cc==c).length}))
             .sort((a,b) => {
                 if (a.count == b.count) {
@@ -49,21 +49,12 @@ class Hand {
                     return b.count - a.count;
                 }
             })
-            .map((c) => c.value);
-
+            .map((c) => c.value);*/
 
         const [type, typeRank] = Hand.types[uniqueCount];
-
-        console.log('*********');
-        console.log({ cardChars: cardChars.join`` });
-        console.log({ uniqueCardChars: uniqueCardChars.join`` });
-        console.log({ uniqueCount: uniqueCount });
-        console.log({ typeRank:typeRank, type:type});
-        console.log({ cardRanks:cardRanks.join`-`});
-        console.log('');
         
-
-        return [type, typeRank, cardRanks.concat(Array(5 - cardRanks.length).fill(0))];
+        //return [type, typeRank, cardRanks.concat(Array(5 - cardRanks.length).fill(0))];
+        return [type, typeRank, this.cards.map(c => c.value)];
     }
     static types = {
         '5': ['Five of a kind',7],
@@ -102,5 +93,13 @@ input.split`\n`.forEach((inputLine) => hands.push(new Hand(inputLine)));
 
 //sort hands by rank and cardrank
 hands.sort((a,b) => (b.rank - a.rank || b.cardRanks[0] - a.cardRanks[0] || b.cardRanks[1] - a.cardRanks[1] || b.cardRanks[2] - a.cardRanks[2] || b.cardRanks[3] - a.cardRanks[3] || b.cardRanks[4] - a.cardRanks[4] || b.cardRanks[5] - a.cardRanks[5]));
+
+let totalWinnings = 0;
+hands.forEach((el,ix,ar) => {
+    totalWinnings += el.bid * (ar.length-ix);
+    //console.log({rank:ar.length-ix, bid:el.bid, winning:el.bid*(ar.length-ix), hand:el.type, cards:el.cards.map(c => c.char).join``, cardrank:el.cardRanks.join`-`});
+});
+
+console.log({totalWinnings:totalWinnings});
 
 debugger;
